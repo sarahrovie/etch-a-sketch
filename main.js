c47668320e5e28b3ebc9cde715cbe204c88fa989
Sarah <sarahrdvieira@gmail.com>
2025-02-19 17:54:36 -0300

    Add button with event listener that prompts for new grid size and creates a new grid

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const container = document.querySelector("#container");
+const btn = document.querySelector("#grid-btn");
 
 let i = 1;
 
-const createDivs = () => {   
+const createDivs = (num = 16) => {   
 
-    while (i < 17) {
+    while (i <= num) {
         const newDiv = document.createElement("div");
         newDiv.setAttribute("id", i);
         newDiv.setAttribute("class", "grid-div");
@@ -27,3 +28,21 @@ gridDivs.forEach(div => {
     })
 });
 
+const removeAllChildren = (parent) => {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }
+}
+
+btn.addEventListener("click", () => {
+    const number = parseInt(prompt("Number of squares per side:"));
+
+    if (number > 100) {
+        alert("Please input a number below 100!");
+    }
+
+    const numOfDivs = number * number; 
+    
+    removeAllChildren(container);
+    createDivs(numOfDivs);
+})
