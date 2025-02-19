434df0dabb449ccca7875ca25de770074b07a899
Sarah <sarahrdvieira@gmail.com>
2025-02-19 18:41:40 -0300

    Edit createDivs function to create rows of divs

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#grid-btn");
 
+// const createDivs = (num = 17) => {   
+//     let i = 1;
 
-const createDivs = (num = 17) => {   
-    let i = 1;
+//     while (i < num) {
+//         const newDiv = document.createElement("div");
+//         newDiv.setAttribute("id", i);
+//         newDiv.setAttribute("class", "grid-div");
+//         container.appendChild(newDiv);
+//         i++;
+//     }
+// }
 
-    while (i < num) {
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute("id", i);
-        newDiv.setAttribute("class", "grid-div");
-        container.appendChild(newDiv);
-        i++;
+const createDivs = (num = 4) => {
+   for (let i = 0; i < num; i++) {
+        const newRow = document.createElement("div");
+        newRow.setAttribute("class", "grid-row");
+        container.appendChild(newRow);
+        
+        for (let j = 0; j < num; j++) {
+            const newDiv = document.createElement("div");
+            newDiv.setAttribute("class", "grid-div");
+            newRow.appendChild(newDiv);
+        }
     }
 }
+
+createDivs();
 
 btn.addEventListener("click", () => {
     const number = parseInt(prompt("Number of squares per side:"));
@@ -21,10 +36,8 @@ btn.addEventListener("click", () => {
         alert("Please input a number above 1 and below 100!");
     }
 
-    const numOfDivs = (number * number) + 1; 
-
     removeAllChildren(container);
-    createDivs(numOfDivs);
+    createDivs(number);
 })
 
 const gridDivs = document.querySelectorAll(".grid-div");
